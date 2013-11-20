2b43e01631cd95fcd0111b0c3ebb2e04f07825ed
Hiroki Murata <mrthrk@jp.ibm.com>
2013-11-20 07:04:45 +0000

    modify buildp.sh in CoMDparallel/src
    git-svn-id: svn://svn.code.sf.net/p/x10/code/applications/trunk@26877 a326200e-df2c-42e0-81fa-597e909af41d

--- CoMD/CoMDparallel/src/buildp.sh
+++ CoMD/CoMDparallel/src/buildp.sh
@@ -1,1 +1,1 @@
-/share/s72common/opt/x10/x10-2.4.0_mpi_O_NOCHECKS/trunk/x10.dist/bin/x10c++ -x10rt mpi -O -NO_CHECKS -o CoMD-parallel -d tmp-x10c++p comd/*.x10
+x10c++ -x10rt mpi -O -NO_CHECKS -o CoMD-parallel -d tmp-x10c++p comd/*.x10
